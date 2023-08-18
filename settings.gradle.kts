da3919e7157a5e6268d0a55b5bf21bfa9237d101
Nguyen Tran <nguyentran@Nguyens-MacBook-Pro-2.local>
2023-08-18 11:53:18 +0700

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "comp1786-L6-android-communication"
+include(":app")
+ 
